f1bed9a35f9b48abf83d77258b42c73a85417c17
feriel214 <fzarrouki1999@gmail.com>
2025-08-29 18:48:59 +0100

    Testing Objects, IEnumerable, & Dates

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Extensions;
 using NetworkUtility.Ping;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +12,21 @@ namespace NetworkUtility.Tests.PingTests
 {
     public class NetworkServiceTests
     {
+        private readonly NetworkService _service;
+
+        public NetworkServiceTests()
+        {
+            _service = new NetworkService();
+        }
+
         [Fact]
         public void NetworkService_sendPing_ReturnString()
         {
             //Arrange
-            NetworkService service = new NetworkService();
+           
 
             //Act
-            string res = service.sendPing();
+            string res = _service.sendPing();
 
             //Assert
             res.Should().BeEquivalentTo("Ping Sent",res);
@@ -33,14 +42,87 @@ namespace NetworkUtility.Tests.PingTests
         public void NetworkService_pingTimeOut_ReturnSumInteger(int a,int b,int expected) {
 
             //Arrange
-            NetworkService service = new NetworkService();
+          
 
             //Act
-            var result=service.pingTimeOut(a,b);
+            var result=_service.pingTimeOut(a,b);
 
             //Assert
             result.Should().Be(expected);
             result.Should().NotBeInRange(-10000, 0);
         }
+
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDateTime()
+        {
+            //Arrange
+
+            //Act
+
+            var result = _service.LastPingDate();
+
+            //Assert
+            
+            result.Should().BeAfter(1.February(2010));
+            result.Should().BeBefore(2.March(2028));
+        }
+
+
+
+        [Fact]
+        public void NetworkService_GetPingOptions_PingOptions()
+        {
+            //Arrange
+            var expected= new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+
+            var result = _service.GetPingOptions();
+
+            //Assert
+
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+          
+        }
+
+
+
+
+
+
+
+
+
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnIEnumerable()
+        {
+            //Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+
+            var result = _service.MostRecentPings();
+
+            //Assert
+
+            result.Should().BeAssignableTo<IEnumerable<PingOptions>>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment ==true);
+
+        }
+
+
     }
 }
--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,45 @@ namespace NetworkUtility.Ping
         public int pingTimeOut(int a, int b)
         {
             return a + b;
+        }
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+        }
+
+        public IEnumerable<PingOptions> MostRecentPings() {
+
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                        new PingOptions()
+                    {
+                        DontFragment = true,
+                        Ttl = 1
+                    },
+                        new PingOptions()
+                    {
+                        DontFragment = true,
+                        Ttl = 1
+                    },new PingOptions()
+                    {
+                        DontFragment = true,
+                        Ttl = 1
+                    }
+            };
+
+            return pingOptions;
+
         }
     }
 }
